9c24dbc187741f222b5cbdcfb2b84f42f6a552a6
Saad Guessous <guessous_saad@hotmail.com>
2013-08-10 17:17:36 +0300

    Finalazing the main view...

--- app/views/welcome.blade.php
+++ app/views/welcome.blade.php
@@ -12,23 +12,27 @@
 
       <!-- Three columns of text below the carousel -->
       <div class="row">
+        <h1><i class="icon-film"></i>&nbsp;New From Movies:</h1><br />
         @foreach ($movies as $movie)
         <div class="col-lg-4">
           <img class="img-rounded" src="{{$base_url}}/images/products_images/{{$movie->id.'_thumb.jpg'}}" style="width:160;height:220;">
           <h2>{{$movie->product_name}}</h2>
           <p>{{ implode(' ', array_slice( explode(' ', $movie->product_description), 0, 40) ).'...' }}</p>
-          <p><a class="btn btn-default" href="#">View details &raquo;</a></p>
+          <p><a class="btn btn-primary" href="#">Add to cart &raquo;</a>&nbsp;&nbsp;<a class="btn btn-default" href="#">View details &raquo;</a></p>
         </div><!-- /.col-lg-4 -->
         @endforeach
       </div><!-- /.row -->
 
+      <hr class="featurette-divider">
+
       <div class="row">
+       <h1><i class="icon-book"></i>&nbsp;New From Ebooks:</h1><br />
         @foreach ($ebooks as $ebook)
         <div class="col-lg-4">
           <img class="img-rounded" src="{{$base_url}}/images/products_images/{{$ebook->id.'_thumb.jpg'}}" style="width:160;height:220;">
           <h2>{{$ebook->product_name}}</h2>
           <p>{{ implode(' ', array_slice( explode(' ', $ebook->product_description), 0, 40) ).'...' }}</p>
-          <p><a class="btn btn-default" href="#">View details &raquo;</a></p>
+          <p><a class="btn btn-primary" href="#">Add to cart &raquo;</a>&nbsp;&nbsp;<a class="btn btn-default" href="#">View details &raquo;</a></p>
         </div><!-- /.col-lg-4 -->
         @endforeach
       </div><!-- /.row -->
