8564a5142e842a0f74ed783998d5a444f6064ad7
Saad Guessous <guessous_saad@hotmail.com>
2013-09-15 20:09:03 +0300

    Refactored class ManageProductsController, added more methods to divide responsibility between them.

--- app/controllers/Admin/ManageProductsController.php
+++ app/controllers/Admin/ManageProductsController.php
@@ -6,39 +6,40 @@ class ManageProductsController extends BaseController {
 	private $itemsPerPage = 12;
 	private $products;
 
+	private $movies;
 	private $moviesNumPages = 0;
 	private $moviesCount;
 
+	private $ebooks;
 	private $ebooksNumPages = 0;
 	private $ebooksCount;
 
 	private $_cart_data;
 
 	public function __construct() {
+
 		$this->products = new Product;
 		$this->_cart_data = new CartItem;
-		$this->initialize();
 	}
 
 	public function index($moviePage = 1, $ebookPage = 1, $type = 'Dvd')
 	{
-		$movieSkip = ($moviePage - 1) * $this->itemsPerPage;
-		$ebookSkip = ($ebookPage - 1) * $this->itemsPerPage;
+		$this->getMovies($moviePage);
 
-		$movies = $this->products->getMovies('Dvd', $movieSkip, $this->itemsPerPage);
-		
-	    $ebooks = $this->products->getProducts('Book', $ebookSkip, $this->itemsPerPage);
-	    
+	    $this->getEbooks($ebookPage);
 
-		
-		
+	    return $this->displayView($moviePage, $ebookPage, $type);		
+	}
+
+	private function displayView($moviePage, $ebookPage, $type) {
+
 		list( $cart_products, $cart_items_count, $total ) = $this->_cart_data->get_cart_data();
 
         return View::make('admin.update_product', array('cart_items_count' => $cart_items_count,
 							                                          'total' => $total,
 							                                  'cart_products' => $cart_products,
-							                                  	     'movies' => $movies,
-							                                  	     'ebooks' => $ebooks,
+							                                  	     'movies' => $this->movies,
+							                                  	     'ebooks' => $this->ebooks,
 							                                  	     'moviesCount' => $this->moviesCount,
 							                                  	     'ebooksCount' => $this->ebooksCount,
 							                                  	     'moviesNumPages' => $this->moviesNumPages,
@@ -46,14 +47,32 @@ class ManageProductsController extends BaseController {
 							                                  	     'moviePage' => $moviePage,
 							                                         'ebookPage' =>  $ebookPage,
 							                                  	     'type' => $type
-				                                              ));
+				                                              ));	
+
 	}
 
-	private function initialize()
-	{
+	private function getMovies($moviePage) {
+
+		$this->initialize_movies();
+		$movieSkip = ($moviePage - 1) * $this->itemsPerPage;
+		$this->movies = $this->products->getMovies('Dvd', $movieSkip, $this->itemsPerPage);
+	}
+
+	private function getEbooks($ebookPage) {
+
+		$this->initialize_ebooks();
+		$ebookSkip = ($ebookPage - 1) * $this->itemsPerPage;
+		$this->ebooks = $this->products->getProducts('Book', $ebookSkip, $this->itemsPerPage);
+	}
+
+	private function initialize_movies() {
+
 		$this->moviesCount = $this->products->getProductCount('Dvd');
 		$this->moviesNumPages = (int)( $this->moviesCount[0]->count / $this->itemsPerPage );	
 		if ($this->moviesCount[0]->count % $this->itemsPerPage) $this->moviesNumPages += 1;
+	}
+
+	private function initialize_ebooks() {
 
 		$this->ebooksCount = $this->products->getProductCount('Book');
 		$this->ebooksNumPages = (int)( $this->ebooksCount[0]->count / $this->itemsPerPage );	
