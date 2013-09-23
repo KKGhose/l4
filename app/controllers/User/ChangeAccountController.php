bfe130ecee736a973ec6a691fce892fa0b5e1dbb
Saad Guessous <guessous_saad@hotmail.com>
2013-09-23 18:00:26 +0300

    Added index and displayView methods to ChangeAccountController

--- app/controllers/User/ChangeAccountController.php
+++ app/controllers/User/ChangeAccountController.php
@@ -2,13 +2,33 @@
 
 class ChangeAccountController extends BaseController {
 
-	$cart_data = new CartItem;	
-	list( $cart_products, $cart_items_count, $total ) = $cart_data->get_cart_data();
+	private $cart_data;
+
+	public function __construct()
+	{
+		$this->cart_data = new CartItem;
+	}
+
+	public function index($id)
+	{
+		$user = User::find($id);
+
+		return $this->displayView($user);
+	}
+
+	protected function displayView($user)
+	{
+		list( $cart_products, $cart_items_count, $total ) = $this->cart_data->get_cart_data();
 
 
-	return View::make('account.change_account', array('cart_items_count' => $cart_items_count,
-			                                          'total' => $total,
-			                                  'cart_products' => $cart_products
-			                                  ));
+		return View::make('account.change_account', array('cart_items_count' => $cart_items_count,
+				            			                              'total' => $total,
+							                                  'cart_products' => $cart_products,
+							                                  		 'user'	=> $user
+			                            			      ));
+	}
+
+		
+	
 
 }//End class ChangeAccountController
