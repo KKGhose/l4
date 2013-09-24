cac52ccfcbccf6ae985ecedbf885b8370a96769e
Saad Guessous <guessous_saad@hotmail.com>
2013-09-24 14:01:42 +0300

    Refactored controller to use product model.

--- app/controllers/MoviesController.php
+++ app/controllers/MoviesController.php
@@ -8,17 +8,25 @@ class MoviesController extends BaseController {
 	 * @return Response
 	 */
 	private $count = 0;
-	private $items_per_page = 6;
-	private $num_pages = 0;
+	private $itemsPerPage = 6;
+	private $numPages = 0;
+	private $skip;
+
+	private $movies;
+	private $moviesModel; 
 	
 	private $_cart_data;
 	private $_log;
-
+	private $cartProducts;
+	private $cartItemsCount;
+	private $total;
+ 
 	public function __construct()
 	{
 		$this->initialize();
 		$this->_cart_data = new CartItem;
 		$this->_log = new AccessLog;
+		$this->moviesModel = new Product;
 	}
 
 	private function initialize()
@@ -28,9 +36,9 @@ class MoviesController extends BaseController {
 		$this->count = DB::select('SELECT COUNT(*) as cnt FROM products
 			                       WHERE product_type = ?', array( $var[0]->id ) );
 
-		$this->num_pages = (int)( $this->count[0]->cnt / $this->items_per_page );	
+		$this->numPages = (int)( $this->count[0]->cnt / $this->itemsPerPage );	
 
-		if ($this->count[0]->cnt % $this->items_per_page) $this->num_pages += 1;
+		if ($this->count[0]->cnt % $this->itemsPerPage) $this->numPages += 1;
 	}
 
 	public function index($page = 1)
@@ -38,25 +46,38 @@ class MoviesController extends BaseController {
 		
 		$this->_log->save_log($this->_log, 'movies.index');
 
-		$skip = ($page - 1) * $this->items_per_page;
+		$this->skip = ($page - 1) * $this->itemsPerPage;
 
-		$movies = DB::select('SELECT products.*, productTypes.type_name, trailers.code
+		/*$movies = DB::select('SELECT products.*, productTypes.type_name, trailers.code
 							  FROM products INNER JOIN productTypes
 							  INNER JOIN trailers
 							  WHERE products.product_type = productTypes.id
 							  AND trailers.movie_id = products.id 
 							  AND productTypes.type_name LIKE ?  
-							  ORDER BY products.id DESC LIMIT ?, ?', array('Dvd', $skip, $this->items_per_page));
+							  ORDER BY products.id DESC LIMIT ?, ?', array('Dvd', $skip, $this->items_per_page));*/
+		
+
 		
 		list( $cart_products, $cart_items_count, $total ) = $this->_cart_data->get_cart_data();
 
 		return View::make('products.movies', array('movies' => $movies, 
-			                                      'num_pages' => $this->num_pages,
+			                                      'num_pages' => $this->numPages,
 			                               'cart_items_count' => $cart_items_count,
 			                                          'total' => $total,
 			                                  'cart_products' => $cart_products, 
 			                                           'page' => $page ));
 	}
+
+	private function initialize_movies()
+	{
+		$this->movies = $this->$moviesModel->getMovies('Dvd', $this->skip, $this->itemsPerPage);
+	}
+
+	private function initialize_cart()
+	{
+		list( $this->cartProducts, $this->cartItemsCount, $this->total ) = $this->_cart_data->get_cart_data();
+	}
+
 
 
 
